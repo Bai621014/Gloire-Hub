cd884570fbdb64c6e49e5b26333e35938cb7ce4e
Bai621014 <bainimanengandjei@gmail.com>
2026-04-22 22:04:55 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home(){
-return(<div style={{padding:'40px',textAlign:'center',background:'black',color:'gold',minHeight:'100vh'}}>
-<h1>GLOIRE HUB</h1>
-<h2>Chaque mot une parole de la vie divine</h2>
-<p>V1 PAROLE DIVINE - N'Djamena 🇹🇩</p>
-<button style={{marginTop:'30px',padding:'15px 30px',background:'gold',color:'black',border:'none',borderRadius:'8px'}}>ENTRER DANS LA GLOIRE</button>
-</div>)
-}
