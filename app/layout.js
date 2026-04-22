b5f21bd1d9406bff3b1616047bfad4adfb493217
Bai621014 <bainimanengandjei@gmail.com>
2026-04-23 00:45:59 +0100

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'GLOIRE HUB',
+  description: 'Chaque mot une parole de la vie divine',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+    }
